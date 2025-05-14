439aa4b9ef8e08d952c01d86e1ccc66b143343ac
Angel <angel.santiago@tutanota.com>
2025-05-14 18:34:24 -0400

    Hardcode websockets url

--- apps/web/src/shared/trpc.ts
+++ apps/web/src/shared/trpc.ts
@@ -1,7 +1,8 @@
 import { createWSClient, wsLink, createTRPCClient } from '@trpc/client';
 import type { AppRouter } from '@abuddy/api';   // ← Type‑only!
 
-const ws = createWSClient({ url: import.meta.env.VITE_API_WS });
+// const ws = createWSClient({ url: import.meta.env.VITE_API_WS });
+const ws = createWSClient({ url: 'ws://localhost:3001/trpc' });
 export const trpc = createTRPCClient<AppRouter>({
   links: [wsLink({ client: ws })],
 });
