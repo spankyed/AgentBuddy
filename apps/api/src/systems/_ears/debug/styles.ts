c16e3cf9f98a4ea4b21e1ca8aea716d5655122c0
Angel <angel.santiago@tutanota.com>
2025-05-19 14:25:58 -0400

    Code cleanup

--- apps/api/src/systems/_ears/debug/styles.ts
+++ apps/api/src/systems/_ears/debug/styles.ts
@@ -46,21 +46,6 @@ export const internals = {
   },
 };
 
-const ansiToBlessedMap = {
-  '\x1b[44m\x1b[37m': '{#000080-bg}{#ffffff-fg}', // Dark blue background with white text
-  '\x1b[104m\x1b[34m': '{#add8e6-bg}{#000080-fg}', // Light blue background with dark blue text
-  '\x1b[42m\x1b[97m': '{#228b22-bg}{#ffffff-fg}',
-  '\x1b[102m\x1b[32m': '{#90ee90-bg}{#006400-fg}',
-  '\x1b[40m\x1b[97m': '{#000000-bg}{#ffffff-fg}',
-  '\x1b[100m\x1b[30m': '{#34568b-bg}{#e0e0e0-fg}', // AA secondary
-  '\x1b[43m\x1b[97m': '{#ffd700-bg}{#ffffff-fg}',
-  '\x1b[103m\x1b[33m': '{#ffffe0-bg}{#d4af37-fg}',
-  '\x1b[41m\x1b[97m': '{#b22222-bg}{#ffffff-fg}',
-  '\x1b[101m\x1b[31m': '{#f08080-bg}{#8b0000-fg}',
-  '\x1b[47m\x1b[30m': '{#ffffff-bg}{#000000-fg}',
-  '\x1b[107m\x1b[97m': '{#f8f8f8-bg}{#000000-fg}'
-};
-
 const defaultStyles = {
   reset: '\x1b[0m',
   bold: '\x1b[1m',
