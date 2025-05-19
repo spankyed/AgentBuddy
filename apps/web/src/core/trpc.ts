f1f016f823e224132e4a54452bab1a921b6f45e5
Angel <angel.santiago@tutanota.com>
2025-05-19 19:57:38 -0400

    Code cleanup

--- apps/web/src/core/trpc.ts
+++ apps/web/src/core/trpc.ts
@@ -1,5 +1,5 @@
 import { createWSClient, wsLink, createTRPCClient } from '@trpc/client';
-import type { AppRouter } from '@abuddy/api';   // ← Type‑only!
+import type { AppRouter } from '@abuddy/api';   // ← BE import Type‑only!
 
 const ws = createWSClient({ url: import.meta.env.VITE_API_WS });
 export const trpc = createTRPCClient<AppRouter>({
