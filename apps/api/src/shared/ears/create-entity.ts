d41712a85213c19d5f7681c47865acd9de757d83
Angel <angel.santiago@tutanota.com>
2025-05-19 20:33:51 -0400

    Use randomId when creating entities

--- apps/api/src/shared/ears/create-entity.ts
+++ apps/api/src/shared/ears/create-entity.ts
@@ -1,19 +1,6 @@
 // import { logInternal } from "../debug/Log";
+import { randomId } from "../utils/random-id";
 import type { EARS } from "./types";
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-const id_counters: Record<EARS.Entity, number> = {} as any; // todo use uuid and/or persistent storage
-
-const createEntity = (type: EARS.Entity, skipLog?: boolean): EARS.EntityId => {
-  id_counters[type] = (id_counters[type] || 0) + 1;
-
-  const newEntity = `${type}-${id_counters[type]}` as EARS.EntityId;
-
-  // logInternal('EC', skipLog, newEntity);
-
-  return newEntity;
-};
-
-export {
-  createEntity
-};
+export const createEntity = (t: EARS.Entity) =>
+  `${t}-${randomId()}` as EARS.EntityId;
