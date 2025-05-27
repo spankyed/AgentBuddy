9725cf952c8ec725e38270f4b96d08ae7d583bc7
Angel <angel.santiago@tutanota.com>
2025-05-26 21:17:42 -0400

    Move tags above threads in view.vue

--- apps/web/src/plugins/threads/canvas/view.vue
+++ apps/web/src/plugins/threads/canvas/view.vue
@@ -34,36 +34,6 @@
         </div>
       </div>
 
-      <!-- Threads list -->
-      <div>
-        <!-- <Label>Related Threads</Label> -->
-        <div class="flex flex-wrap gap-2">
-          <button
-            type="button"
-            @click="() => actor.send({ type: 'LINK_THREAD' })"
-            class="flex items-center gap-2 px-4 py-2 mr-2 text-sm font-medium transition-colors rounded h-7 text-neutral-200 bg-neutral-900/60 hover:bg-neutral-700"
-          >
-            Link Thread
-            <Plus :size="16" class="text-neutral-300" />
-          </button>
-
-          <span
-            v-for="(thread, index) in relatedThreads"
-            :key="index"
-            class="cursor-pointer inline-flex items-center pl-3 py-0.5 text-sm bg-neutral-900/60 text-neutral-200 rounded"
-          >
-            {{ thread.shortCode }}
-            <button
-              type="button"
-              @click="() => actor.send({ type: 'REMOVE_LINK', index })"
-              class="p-1 ml-1 rounded focus:outline-none"
-            >
-              <X :size="16" class="text-neutral-400 hover:text-neutral-200" />
-            </button>
-          </span>
-        </div>
-      </div>
-
       <!-- Tags list -->
       <div>
         <!-- <Label>Tags</Label> -->
@@ -94,6 +64,36 @@
         </div>
       </div>
 
+      <!-- Threads list -->
+      <div>
+        <!-- <Label>Related Threads</Label> -->
+        <div class="flex flex-wrap gap-2">
+          <button
+            type="button"
+            @click="() => actor.send({ type: 'LINK_THREAD' })"
+            class="flex items-center gap-2 px-4 py-2 mr-2 text-sm font-medium transition-colors rounded h-7 text-neutral-200 bg-neutral-900/60 hover:bg-neutral-700"
+          >
+            Link Thread
+            <Plus :size="16" class="text-neutral-300" />
+          </button>
+
+          <span
+            v-for="(thread, index) in relatedThreads"
+            :key="index"
+            class="cursor-pointer inline-flex items-center pl-3 py-0.5 text-sm bg-neutral-900/60 text-neutral-200 rounded"
+          >
+            {{ thread.shortCode }}
+            <button
+              type="button"
+              @click="() => actor.send({ type: 'REMOVE_LINK', index })"
+              class="p-1 ml-1 rounded focus:outline-none"
+            >
+              <X :size="16" class="text-neutral-400 hover:text-neutral-200" />
+            </button>
+          </span>
+        </div>
+      </div>
+
       <!-- Messages Container -->
       <div class="mt-5">
         <div class="flex gap-2 pb-2">
@@ -114,10 +114,10 @@
             type="submit"
             @click="actor.send({ type: 'GO_BACK' })"
             :class="[
-              'ml-auto px-4 py-2 h-7 rounded text-sm font-medium transition-colors flex items-center gap-2 hover:bg-neutral-700 text-neutral-500 hover:text-white',
+              'ml-auto pl-2 pr-3 py-2 h-7 rounded text-sm font-medium transition-colors flex items-center gap-2 hover:bg-neutral-700 text-neutral-500 hover:text-white',
             ]"
           >
-            <ArrowLeft :size="22" />
+            <ArrowLeft :size="16" />
             Back
           </button>
           <Button 
