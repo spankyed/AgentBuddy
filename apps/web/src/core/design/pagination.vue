7f208174cefecddd6ea6df632e656e55c351da09
Angel <angel.santiago@tutanota.com>
2025-05-27 02:23:28 -0400

    Update pagination UI with consistent dark theme and smaller icons

--- apps/web/src/core/design/pagination.vue
+++ apps/web/src/core/design/pagination.vue
@@ -4,21 +4,23 @@
       :page="currentPage"
       :itemsPerPage="itemsPerPage"
       :total="total"
+      :siblingCount="siblingCount"
+      show-edges
       @update:page="handlePageChange"
     >
-      <PaginationList v-slot="{ items }" class="flex items-center gap-1 text-stone-700 dark:text-white">
-        <PaginationFirst class="flex items-center justify-center transition bg-transparent rounded-lg w-9 h-9 hover:bg-white dark:hover:bg-stone-700/70 disabled:opacity-50 dark:disabled:opacity-30" :disabled="isDisabled || currentPage === 1">
-          <ChevronsRight class="rotate-180" />
+      <PaginationList v-slot="{ items }" class="flex items-center gap-1 text-white">
+        <PaginationFirst class="flex items-center justify-center transition bg-transparent rounded-lg w-9 h-9 hover:bg-neutral-700/40 disabled:opacity-30" :disabled="isDisabled || currentPage === 1">
+          <ChevronsRight class="rotate-180" :size="16" />
         </PaginationFirst>
-        <PaginationPrev class="flex items-center justify-center mr-4 transition bg-transparent rounded-lg w-9 h-9 hover:bg-white dark:hover:bg-stone-700/70 disabled:opacity-50 dark:disabled:opacity-30" :disabled="isDisabled || currentPage === 1">
-          <ChevronRight class="rotate-180" />
+        <PaginationPrev class="flex items-center justify-center mr-4 transition bg-transparent rounded-lg w-9 h-9 hover:bg-neutral-700/40 disabled:opacity-30" :disabled="isDisabled || currentPage === 1">
+          <ChevronRight class="rotate-180" :size="16" />
         </PaginationPrev>
         
         <template v-for="(page, index) in items">
           <PaginationListItem
             v-if="page.type === 'page'"
             :key="index"
-            class="w-9 h-9 border dark:border-stone-800 rounded-lg data-[selected]:!bg-white dark:data-[selected]:!bg-stone-700 data-[selected]:shadow-sm data-[selected]:text-blackA11 dark:data-[selected]:text-white hover:bg-white dark:hover:bg-stone-700/70 transition"
+            class="w-9 h-9 border border-neutral-700/60 rounded-lg data-[selected]:!bg-neutral-700 data-[selected]:shadow-sm data-[selected]:text-white hover:bg-neutral-700/40 transition"
             :value="page.value"
             :disabled="isDisabled"
           >
@@ -34,11 +36,11 @@
           </PaginationEllipsis>
         </template>
         
-        <PaginationNext class="flex items-center justify-center ml-4 transition bg-transparent rounded-lg w-9 h-9 hover:bg-white dark:hover:bg-stone-700/70 disabled:opacity-50 dark:disabled:opacity-30" :disabled="isDisabled || currentPage === totalPages">
-          <ChevronRight />
+        <PaginationNext class="flex items-center justify-center ml-4 transition bg-transparent rounded-lg w-9 h-9 hover:bg-neutral-700/40 disabled:opacity-30" :disabled="isDisabled || currentPage === totalPages">
+          <ChevronRight :size="16" />
         </PaginationNext>
-        <PaginationLast class="flex items-center justify-center transition bg-transparent rounded-lg w-9 h-9 hover:bg-white dark:hover:bg-stone-700/70 disabled:opacity-50 dark:disabled:opacity-30" :disabled="isDisabled || currentPage === totalPages">
-          <ChevronsRight />
+        <PaginationLast class="flex items-center justify-center transition bg-transparent rounded-lg w-9 h-9 hover:bg-neutral-700/40 disabled:opacity-30" :disabled="isDisabled || currentPage === totalPages">
+          <ChevronsRight :size="16" />
         </PaginationLast>
       </PaginationList>
     </PaginationRoot>
