74dad6c11d68ce4aa96eef31ad0613772eb42271
Angel <angel.santiago@tutanota.com>
2025-05-28 16:28:58 -0400

    refactor: rename ThreadsViewData type to ThreadViewData for consistency

--- apps/api/src/systems/threads/types.ts
+++ apps/api/src/systems/threads/types.ts
@@ -1,6 +1,6 @@
 import type { EARS, MessageEntity, TagEntity, ThreadEntity } from "@/types";
 
-export type ThreadsViewData = {
+export type ThreadViewData = {
   messages?: Partial<MessageEntity>[];
   relatedThreads?: Partial<ThreadEntity>[];
   tags?: Partial<TagEntity>[];
