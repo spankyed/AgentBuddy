1f7a1a4f9eeb519423a72cf81bb8ef30e4e9a7e9
Angel <angel.santiago@tutanota.com>
2025-06-14 00:54:05 -0400

    Add capitalize first letter util

--- apps/web/src/core/utils.ts
+++ apps/web/src/core/utils.ts
@@ -1,2 +1,6 @@
 export const entries = <T extends Record<string, unknown>>(obj: T) =>
   Object.entries(obj) as Array<[keyof T, T[keyof T]]>
+
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
