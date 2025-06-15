e9ffa632807081287702359ef955a458396f1928
Angel <angel.santiago@tutanota.com>
2025-06-14 23:30:25 -0400

    Fix layout

--- apps/web/src/plugins/database/canvas/canvas.vue
+++ apps/web/src/plugins/database/canvas/canvas.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="flex h-full w-full">
+  <div class="flex w-full h-full overflow-hidden">
     <!-- Schema Panel -->
-    <div class="w-80 border-r border-gray-200 dark:border-gray-700 overflow-y-auto">
+    <div class="flex-shrink-0 w-64 overflow-y-auto border-r border-gray-200 dark:border-gray-700">
       <SchemaPanel />
     </div>
     
     <!-- Main Content Area -->
-    <div class="flex-1 flex flex-col">
+    <div class="flex flex-1 min-w-0">
       <!-- Query Editor -->
-      <div class="h-2/5 border-b border-gray-200 dark:border-gray-700">
+      <div class="w-1/2 h-full overflow-y-auto border-r border-gray-200 dark:border-gray-700">
         <QueryEditor />
       </div>
       
       <!-- Graph Explorer -->
-      <div class="flex-1">
+      <div class="w-1/2 h-full overflow-hidden">
         <GraphExplorer />
       </div>
     </div>
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import SchemaPanel from './SchemaPanel.vue';
-import QueryEditor from './QueryEditor.vue';
-import GraphExplorer from './GraphExplorer.vue';
+import SchemaPanel from './SchemaPanel.vue'
+import QueryEditor from './QueryEditor.vue'
+import GraphExplorer from './GraphExplorer.vue'
 </script> 
