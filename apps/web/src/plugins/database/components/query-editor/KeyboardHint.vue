a0107e8ac4fc7c1e9129cdcd80977fa66695ae24
Angel <angel.santiago@tutanota.com>
2025-06-16 10:57:51 -0400

    Code cleanup

--- apps/web/src/plugins/database/components/query-editor/KeyboardHint.vue
+++ apps/web/src/plugins/database/components/query-editor/KeyboardHint.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="items-center hidden gap-1 mr-2 text-xs text-gray-500 sm:flex dark:text-gray-400">
     <Keyboard class="w-3 h-3" />
-    <span>{{ isMac ? 'Cmd' : 'Ctrl' }}+Enter to run</span>
+    <span>{{ isMac ? 'Cmd' : 'Ctrl' }} + Enter to run</span>
   </div>
 </template>
 
