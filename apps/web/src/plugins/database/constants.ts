26d56d67748b173c283ccbfa0d0777faef2c5bef
Angel <angel.santiago@tutanota.com>
2025-07-01 20:56:59 -0400

    Rearrange query template data

--- apps/web/src/plugins/database/constants.ts
+++ apps/web/src/plugins/database/constants.ts
@@ -38,11 +38,11 @@ for (const entityId of allEntities) {
     const allData = getAll(entityId);
     
     results.push({
+      targets: targets.slice(0, 5),
       ...entityData,
+      targetCount: targets.length,
       ...allData,
       relationType: '${value}',
-      targetCount: targets.length,
-      targets: targets.slice(0, 5)
     });
   }
 }
