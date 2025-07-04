03a1bc9883199bb73fdd2e27a315e9fc2ef9597f
Angel <angel.santiago@tutanota.com>
2025-07-04 01:39:23 -0400

    refactor: remove header and adjust padding in inspection panel

--- apps/web/src/core/layout/inspection-panel.vue
+++ apps/web/src/core/layout/inspection-panel.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="flex flex-col h-full border-l w-[30rem] bg-neutral-900 border-neutral-800">
-    <div class="flex items-center p-4 border-b border-neutral-800">
-      <!-- <button
+    <!-- <div class="flex items-center p-4 border-b border-neutral-800">
+      <button
         @click="$emit('panel-back')"
         class="flex gap-1 px-2 py-1 text-xs tracking-wider uppercase transition-colors rounded-lg items hover:bg-neutral-700 text-neutral-500 hover:text-white"
       >
         <ChevronLeft :size="18" />
         Back
-      </button> -->
+      </button>
 
-      <!-- ▸ Inspection‑toggle button (right) (disabled 6/1/25) -->
-      <!-- <button 
+      <button 
         @click="$emit('panel-toggle')"
         class="flex items-center gap-1 px-2 py-1 ml-auto text-xs tracking-wider uppercase transition-colors rounded-lg hover:bg-neutral-700 text-neutral-500 hover:text-white"
       >
         <ChevronLeft :size="14" />
         {{ label }}
         <ChevronRight :size="14" />
-      </button> -->
-    </div>
+      </button>
+    </div> -->
     
-    <div class="flex-grow p-8 overflow-y-auto">
+    <div class="flex-grow overflow-y-auto">
       <slot />
     </div>
   </div>
