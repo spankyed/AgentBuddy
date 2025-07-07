6688843891dfa295da4dd38c88c88dd73c42466d
Angel <angel.santiago@tutanota.com>
2025-07-07 19:01:21 -0400

    refactor: convert node form close button to be completely clickable

--- apps/web/src/plugins/flows/canvas/components/NodeForm.vue
+++ apps/web/src/plugins/flows/canvas/components/NodeForm.vue
@@ -13,13 +13,11 @@
     >
       <!-- Tab hanging off the side -->
       <div class="relative">
-        <div class="absolute flex items-center gap-2 px-3 py-2 border border-r-0 rounded-l-lg right-full top-10 bg-neutral-900 border-neutral-800">
-          <button
-            @click="$emit('close')"
-            class="p-1 mr-auto transition-colors rounded hover:bg-neutral-700"
-          >
-            <X class="w-4 h-4 text-neutral-400 hover:text-neutral-200" />
-          </button>
+        <div 
+          @click="$emit('close')"
+          class="absolute flex items-center gap-2 px-3 py-2 border border-r-0 rounded-l-lg right-full top-10 bg-neutral-900 border-neutral-800 cursor-pointer transition-colors hover:bg-neutral-800"
+        >
+          <X class="w-4 h-4 text-neutral-400" />
           <span class="text-sm font-semibold text-neutral-100">{{ selectedNode.nodeType.toUpperCase() }}</span>
         </div>
       </div>
