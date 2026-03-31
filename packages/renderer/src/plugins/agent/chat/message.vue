e9c1beb2563a2f93852e04e57692cff2dbc1d32b
Angel <angel.santiago@tutanota.com>
2026-03-30 21:28:10 -0400

    fix: align attached images to the right in chat message component

--- packages/renderer/src/plugins/agent/chat/message.vue
+++ packages/renderer/src/plugins/agent/chat/message.vue
@@ -60,7 +60,7 @@
       </div>
 
       <!-- Attached images -->
-      <div v-if="message.images?.length" class="flex flex-wrap gap-2 mb-2">
+      <div v-if="message.images?.length" class="flex flex-wrap gap-2 mb-2 justify-end">
         <img v-for="(img, index) in message.images" :key="index" :src="img"
           class="w-20 h-20 object-cover rounded-lg cursor-pointer hover:opacity-80 transition-opacity"
           @click="$emit('open-lightbox', img)" />
