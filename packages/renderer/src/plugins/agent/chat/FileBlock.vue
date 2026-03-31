6da11b21c240cf134e5067cb5195871334b98387
Angel <angel.santiago@tutanota.com>
2026-03-31 03:14:13 -0400

    style: update border opacity for image thumbnails in FileBlock and ImageThumbnail components

--- packages/renderer/src/plugins/agent/chat/FileBlock.vue
+++ packages/renderer/src/plugins/agent/chat/FileBlock.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="relative group flex items-center gap-2.5 w-[240px] bg-neutral-900 border border-neutral-700 rounded-lg p-2 cursor-pointer" @click="openInExplorer">
+  <div class="relative group flex items-center gap-2.5 w-[240px] bg-neutral-900 border border-neutral-700/40 rounded-lg p-2 cursor-pointer" @click="openInExplorer">
     <div class="w-10 h-10 flex-shrink-0 rounded overflow-hidden bg-neutral-800 flex items-center justify-center">
-      <img v-if="file.isImage && (file.previewUrl || loadedPreview)" :src="file.previewUrl || loadedPreview" class="w-full h-full object-cover border border-neutral-700/50" />
+      <img v-if="file.isImage && (file.previewUrl || loadedPreview)" :src="file.previewUrl || loadedPreview" class="w-full h-full object-cover border border-neutral-700/20" />
       <FileIcon v-else :size="20" class="text-neutral-400" />
     </div>
     <div class="flex-1 min-w-0">
