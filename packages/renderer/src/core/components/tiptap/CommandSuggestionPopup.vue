d6d4b46d46323313b5882d06a3e95373692148ec
Angel <angel.santiago@tutanota.com>
2026-03-31 19:45:19 -0400

    feat: add space key command selection in command suggestion popup

--- packages/renderer/src/core/components/tiptap/CommandSuggestionPopup.vue
+++ packages/renderer/src/core/components/tiptap/CommandSuggestionPopup.vue
@@ -135,6 +135,17 @@ function handleKeyDown(event: KeyboardEvent) {
       }
       break
 
+    // Space key selects command
+    // case ' ':
+    //   {
+    //     const cmd = filteredCommands.value[selectedIndex.value]
+    //     if (cmd) {
+    //       event.preventDefault()
+    //       selectCommand(cmd)
+    //     }
+    //   }
+    //   break
+
     case 'Escape':
       event.preventDefault()
       deactivateAndClean()
