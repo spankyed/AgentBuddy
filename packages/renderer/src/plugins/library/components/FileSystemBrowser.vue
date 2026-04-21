d1787d2cbcdd3952dd5de7f7f7e5f82ad6cdbf4e
Angel <angel.santiago@tutanota.com>
2026-04-21 00:10:53 -0400

    Close symlink folder form on outside click

--- packages/renderer/src/plugins/library/components/FileSystemBrowser.vue
+++ packages/renderer/src/plugins/library/components/FileSystemBrowser.vue
@@ -218,6 +218,7 @@
           </Button>
           <div
             v-if="symlinkInput.show"
+            ref="symlinkFormRef"
             class="absolute top-full right-0 mt-2 z-50 flex items-center gap-2 p-3 rounded-md border border-neutral-700 bg-neutral-800 shadow-lg"
           >
             <input
@@ -501,6 +502,26 @@ const deleteDialog = reactive({
 
 const folderMenuOpen = ref(false)
 const symlinkInput = reactive({ show: false, path: '' })
+const symlinkFormRef = ref<HTMLElement | null>(null)
+
+function onSymlinkClickOutside(event: MouseEvent) {
+  if (symlinkInput.show && symlinkFormRef.value && !symlinkFormRef.value.contains(event.target as Node)) {
+    symlinkInput.show = false
+  }
+}
+
+watch(() => symlinkInput.show, (show) => {
+  if (show) {
+    // Use nextTick + setTimeout to avoid the opening click from immediately closing the form
+    setTimeout(() => document.addEventListener('click', onSymlinkClickOutside), 0)
+  } else {
+    document.removeEventListener('click', onSymlinkClickOutside)
+  }
+})
+
+onUnmounted(() => {
+  document.removeEventListener('click', onSymlinkClickOutside)
+})
 
 // Breadcrumb inline editing (reuse composable)
 const {
