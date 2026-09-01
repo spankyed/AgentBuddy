8f3b312e978218250f59f43af931d80e787f4c75
Angel <angel.santiago@tutanota.com>
2026-09-01 11:54:30 -0400

    refactor(build): remove cross-package path aliases to default-setup

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -25,8 +25,6 @@ export default defineConfig(async () => {
 
   resolve: {
     alias: [
-      { find: '@/plugins', replacement: new URL('../default-setup/src/plugins', import.meta.url).pathname },
-      { find: '@/registries', replacement: new URL('../default-setup/src/registries', import.meta.url).pathname },
       { find: /^@\//, replacement: new URL('./src/', import.meta.url).pathname },
     ],
   },
