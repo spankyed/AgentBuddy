ddafd7df70517f3513222ef6a0adf37d2a7b5988
Angel <angel.santiago@tutanota.com>
2026-09-08 16:55:57 -0400

    refactor(steps): rename flow step import path to subflow

--- packages/default-setup/src/extensions/steps/register-fe.ts
+++ packages/default-setup/src/extensions/steps/register-fe.ts
@@ -5,7 +5,7 @@ import { switchStepFE } from './switch/fe';
 import { fireStepFE } from './fire/fe';
 import { transformStepFE } from './transform/fe';
 import { queryStepFE } from './query/fe';
-import { flowStepFE } from './flow/fe';
+import { flowStepFE } from './subflow/fe';
 import { createStepFE } from './create/fe';
 import { updateStepFE } from './update/fe';
 import { keepAliveStepFE } from './keep-alive/fe';
